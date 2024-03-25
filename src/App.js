d7aca973a1d09e0f565fe252a1ddaf34031b602f
Vishvadeep <vishvadeep.0001@gmail.com>
2024-03-25 22:09:57 +0530

    Login and signup page ui created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
-import ProductList from "./features/product/ProductList";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage></SignupPage>,
+  },
+]);
 
 function App() {
-  return <ProductList />;
+  return <RouterProvider router={router}></RouterProvider>
 }
 
 export default App;
